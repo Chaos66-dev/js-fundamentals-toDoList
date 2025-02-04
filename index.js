6ecb5e49ade72ee94d92900cdcebd7bb44e3088b
Chaoss66-dev <erik.voss@spaceforce.mil>
2025-02-04 09:45:07 -0800

    basic js functionality for add/remove button

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 console.log('My code is running');
+var list_item_counter = 4;
+
+function addItem() {
+    let listWrapper = document.getElementsByClassName("list-wrapper")[0];
+    let newItem = document.createElement('div');
+    let newInput = document.createElement('input');
+    let newSpan = document.createElement('span')
+
+    newItem.classList.add("list-item");
+    newInput.setAttribute("type", "checkbox");
+    newInput.setAttribute("name", "list" + list_item_counter.toString());
+    newInput.setAttribute("id", "list" + list_item_counter.toString());
+    newSpan.innerHTML = "Testing"
+    list_item_counter++;
+
+    newItem.appendChild(newInput);
+    newItem.appendChild(newSpan);
+    listWrapper.appendChild(newItem);
+
+};
+
+function removeItem() {
+    alert('remove-item clicked');
+
+    let listWrapper = document.getElementsByClassName("list-wrapper")[0];
+    let childToRemove = listWrapper.children[0]; // make this a variable
+    listWrapper.removeChild(childToRemove);
+};
+
+document.getElementById("add-item").addEventListener("click", addItem);
+document.getElementById("remove-item").addEventListener("click", removeItem);
+
